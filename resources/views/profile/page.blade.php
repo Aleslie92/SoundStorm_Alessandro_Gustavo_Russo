815efd6af3405b2eb758567167bce4e4d542ab59
Alessandro G. Russo <alessandrogustavorusso@gmail.com>
2024-12-05 18:59:37 +0100

    piccoli aggiustamenti frontend User Story 1-2

--- resources/views/profile/page.blade.php
+++ resources/views/profile/page.blade.php
@@ -1,8 +1,8 @@
 <x-layout>
     
-    <div class="container">
+    <div class="container mt-5">
         <div class="row">
-            <div class="col-12">
+            <div class="col-12 text-center">
                 <h1>Pagina Profilo</h1>
             </div>
         </div>
@@ -12,9 +12,9 @@
     
     
     <div class="container mt-5">
-        <div class="row justify-content-evenly">
-            <div class="col-12 col-md-4 p-5">
-                <img class="img-fluid" src={{$user->profile->avatar ? Storage::url($user->profile->avatar) : '/media/avatarDefault.png'}} alt="Immagine di Default">
+        <div class="row justify-content-evenly align-items-center bg-info rounded">
+            <div class="col-12 col-md-4 pt-5 pb-5 d-flex flex-column justify-content-center mx-auto">
+                <img class="img-fluid mx-auto" style="max-height: 400px;" src={{$user->profile->avatar ? Storage::url($user->profile->avatar) : '/media/avatarDefault.png'}} alt="Immagine di Default">
                 @if(session('success'))
                 <div class="container">
                     <div class="row">
@@ -46,99 +46,100 @@
                     
                 </form>
             </div>
-        </div>
-        <div class="col-12 col-md-4">
-            <div class="row">
-                <div class="col-12 col-sm-3 fw-bold">
-                    <p>Nome</p>
+            
+            <div class="col-12 col-md-4 pt-5 pb-5 ">
+                <div class="row">
+                    <div class="col-12 col-sm-3 fw-bold">
+                        <p>Nome</p>
+                    </div>
+                    <div class="col-12 col-sm-9">
+                        <p>{{$user->name ?? ''}}</p>
+                    </div>
                 </div>
-                <div class="col-12 col-sm-9">
-                    <p>{{$user->name ?? ''}}</p>
+                <div class="row">
+                    <div class="col-12 col-sm-3 fw-bold">
+                        <p>Email</p>
+                    </div>
+                    <div class="col-12 col-sm-9">
+                        <p>{{$user->email ?? ''}}</p>
+                    </div>
                 </div>
+                <div class="row">
+                    <div class="col-12 col-sm-3 fw-bold">
+                        <p>Residenza</p>
+                    </div>
+                    <div class="col-12 col-sm-9">
+                        <p>{{$user->profile->address ?? ''}}</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-sm-3 fw-bold">
+                        <p>Città</p>
+                    </div>
+                    <div class="col-12 col-sm-9">
+                        <p>{{$user->profile->city ?? '' }}</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-sm-3 fw-bold">
+                        <p>Provincia</p>
+                    </div>
+                    <div class="col-12 col-sm-9">
+                        <p>{{$user->profile->province ?? ''}}</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-sm-3 fw-bold">
+                        <p>Regione</p>
+                    </div>
+                    <div class="col-12 col-sm-9">
+                        <p>{{$user->profile->region ?? ''}}</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-sm-3 fw-bold">
+                        <p>Paese</p>
+                    </div>
+                    <div class="col-12 col-sm-9">
+                        <p>{{$user->profile->country ?? ''}}</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-sm-3 fw-bold">
+                        <p>CAP</p>
+                    </div>
+                    <div class="col-12 col-sm-9">
+                        <p>{{$user->profile->zip ?? ''}}</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-sm-3 fw-bold">
+                        <p>Numero di Cellulare</p>
+                    </div>
+                    <div class="col-12 col-sm-9">
+                        <p>{{$user->profile->phone ?? ''}}</p>
+                    </div>
+                </div>
+                <a class="btn btn-warning" href="{{route('profile.edit',compact('user'))}}">Modifica Profilo</a>
             </div>
-            <div class="row">
-                <div class="col-12 col-sm-3 fw-bold">
-                    <p>Email</p>
-                </div>
-                <div class="col-12 col-sm-9">
-                    <p>{{$user->email ?? ''}}</p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 col-sm-3 fw-bold">
-                    <p>Residenza</p>
-                </div>
-                <div class="col-12 col-sm-9">
-                    <p>{{$user->profile->address ?? ''}}</p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 col-sm-3 fw-bold">
-                    <p>Città</p>
-                </div>
-                <div class="col-12 col-sm-9">
-                    <p>{{$user->profile->city ?? '' }}</p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 col-sm-3 fw-bold">
-                    <p>Provincia</p>
-                </div>
-                <div class="col-12 col-sm-9">
-                    <p>{{$user->profile->province ?? ''}}</p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 col-sm-3 fw-bold">
-                    <p>Regione</p>
-                </div>
-                <div class="col-12 col-sm-9">
-                    <p>{{$user->profile->region ?? ''}}</p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 col-sm-3 fw-bold">
-                    <p>Paese</p>
-                </div>
-                <div class="col-12 col-sm-9">
-                    <p>{{$user->profile->country ?? ''}}</p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 col-sm-3 fw-bold">
-                    <p>CAP</p>
-                </div>
-                <div class="col-12 col-sm-9">
-                    <p>{{$user->profile->zip ?? ''}}</p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 col-sm-3 fw-bold">
-                    <p>Numero di Cellulare</p>
-                </div>
-                <div class="col-12 col-sm-9">
-                    <p>{{$user->profile->phone ?? ''}}</p>
-                </div>
-            </div>
-            <a class="btn btn-warning" href="{{route('profile.edit',compact('user'))}}">Modifica Profilo</a>
         </div>
     </div>  
     
-    <div class="container mt-5">
-        <div class="row">
+    <div class="container mt-5 bg-info rounded p-3">
+        <div class="row justify-content-center text-center mb-3 ">
             <h2>I Miei Brani</h2>
         </div>
-        <div class="row">
+        <div class="row justify-content-evenly">
             @foreach ($user->tracks as $track)
-            <div class="col-12 col-md-3">
-                <div class="card">
+            <div class="col-12 col-md-3 overflow-auto" style="max-height: 350px;">
+                <div class="card ">
                     <div class="text-center">
-                        <img class="img-fluid" src={{Storage::url($track->cover)}} alt="{{$track->title}}">
+                        <img width="300" class="img-fluid" style="max-height: 150px;background-size: cover;" src={{Storage::url($track->cover)}} alt="{{$track->title}}">
                     </div>
                     <div class="card-body">
                         <h5 class="card-title">{{$track->title}}</h5>
-                        <p class="card-text">{{$track->description}}</p>
-                        <div>
+                        <p class="card-text ">{{$track->description}}</p>
+                        <div class="">
                             <audio class="w-100" controls>
                                 <source src="{{Storage::url($track->path)}}" type="audio/mpeg">
                                     Your browser does not support the audio element.
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,18 @@
 <x-layout>
-
-<h1>SoundStorm</h1>
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-12 text-center">
+            <h1>SoundStorm</h1>
+        </div>
+    </div>
+</div>
+<div class="container mt-5">
+    <div class="row ">
+        <div class="col-12 d-flex justify-content-center">
+            <a class="btn btn-outline-danger" href="{{route('track.create')}}">Aggiungi Un Brano</a>
+        </div>
+    </div>
+</div>
 
 @if (session('success'))
     <div class="alert alert-success">
