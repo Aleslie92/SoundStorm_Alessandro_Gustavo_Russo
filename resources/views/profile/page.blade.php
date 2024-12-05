c06b4db0297faef4274dd1fedadbfe3d435fa61c
Alessandro G. Russo <alessandrogustavorusso@gmail.com>
2024-12-05 19:15:02 +0100

    tasto modifica avatar da aggiustare

--- resources/views/profile/page.blade.php
+++ resources/views/profile/page.blade.php
@@ -28,10 +28,10 @@
                     @csrf
                     @method('PUT')
                     <div class="d-flex justify-content-between align-items-center">
-                        <div class="col-9 mb-3">
+                        <div class="col-8 mb-3 me-2">
                             <input type="file" class="form-control" name="avatar" id="avatar">
                         </div>
-                        <div class="col-2">
+                        <div class="col-4 mb-3">
                             <button type="submit" class="btn btn-sm btn-primary">Cambia Avatar</button>
                         </div>
                     </div>
@@ -47,7 +47,7 @@
                 </form>
             </div>
             
-            <div class="col-12 col-md-4 pt-5 pb-5 ">
+            <div class="col-12 col-md-4 pt-5 ">
                 <div class="row">
                     <div class="col-12 col-sm-3 fw-bold">
                         <p>Nome</p>
